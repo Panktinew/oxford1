92f96da2bfc649378e6c00ac3da215d6dd172989
Panktinew <130337976+Panktinew@users.noreply.github.com>
2023-12-14 14:39:36 +0530

    Add files via upload

---
+++ firstProject/src/firstProject/HelloProgram1.java
@@ -0,0 +1,19 @@
+package firstProject;
+
+public class HelloProgram1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Let us learn java");
+     //  declaration --- datatype and value(optional) 
+     int salary = 61000;  //yearly
+     System.out.println( " Annual Salary" +salary);
+     
+    float salary_on_hand ;
+     
+     salary_on_hand  = (float) salary/12;  // type casting
+     System.out.println( "Monthly Salary" +salary_on_hand);
+     
+	}
+
+}
